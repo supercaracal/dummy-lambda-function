83cfcd1b8c5f5e3eee162d7194089d1a234d9568
Taishi Kasuga <kasuga@livesense.co.jp>
2024-01-18 14:21:25 +0900

    fix

--- main.go
+++ main.go
@@ -4,14 +4,9 @@ import (
 	"context"
 	"log"
 	"os"
-	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-)
-
-const (
-	timeout = 15 * time.Second
 )
 
 var (
@@ -32,11 +27,6 @@ func main() {
 
 // Kinesis consumes the records of the stream
 func Kinesis(ctx context.Context, ev events.KinesisEvent) error {
-	logger.Printf("Number of records: %d", len(ev.Records))
-
-	ctx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
-
 	for _, record := range ev.Records {
 		logger.Print(record.Kinesis.Data)
 	}
@@ -46,11 +36,6 @@ func Kinesis(ctx context.Context, ev events.KinesisEvent) error {
 
 // S3 handles the S3 events
 func S3(ctx context.Context, ev events.S3Event) error {
-	logger.Printf("Number of records: %d", len(ev.Records))
-
-	ctx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
-
 	for _, record := range ev.Records {
 		logger.Printf("bucket: %s, object: %s", record.S3.Bucket.Name, record.S3.Object.Key)
 	}
